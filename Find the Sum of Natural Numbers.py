7fa35940bc3d129115b4f3656856561d42299ae1
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2021-04-14 17:38:12 +0530

    Create Find the Sum of Natural Numbers.py

---
+++ Find the Sum of Natural Numbers.py
@@ -0,0 +1,12 @@
+# take input from the user
+num = int(input("Enter a number: "))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
